c0fc4e4ccbdc1f4d4a073be1e025d9bdde5a7320
Lucamilne <lucamilne@googlemail.com>
2025-11-09 13:51:20 +0000

    github pages init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // static HTML export for GitHub Pages
+  images: {
+    unoptimized: true, // GitHub Pages doesn’t support Next.js image optimization
+  },
+  basePath: "/poc",
+  assetPrefix: "/poc/",
 };
 
 export default nextConfig;
